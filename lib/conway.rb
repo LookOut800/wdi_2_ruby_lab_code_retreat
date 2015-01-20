d1b139e0061085d6a68e22c74da2aefb7211c3b9
Charles Talbot <charlietalbot@gmail.com>
2015-01-20 16:34:09 -0500

    changes for silent programming w broc and brandonn

---
+++ lib/conway.rb
@@ -0,0 +1,11 @@
+class Cell
+
+  def initialize(cell)
+    @cell = cell
+  end
+
+# def cell
+#   @cell = cell.new
+# end
+
+end
--- spec/conway_spec.rb
+++ spec/conway_spec.rb
@@ -1,1 +1,23 @@
 require_relative '../lib/conway'
+
+describe Cell do
+
+@cell = Cell.new
+
+
+  describe 'initialize'do
+    it 'describes the object Cell' do
+      expect(@cell).to eq(Cell.new)
+    end
+  end
+
+
+
+
+
+
+
+
+
+
+end
